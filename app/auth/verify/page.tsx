0ab97d3e6e50d295f22e0e97db096b7fdb192e69
Insham Haque <haqueinsham@gmail.com>
2024-09-29 00:36:48 +0530

    some changes

--- app/auth/verify/page.tsx
+++ app/auth/verify/page.tsx
@@ -2,7 +2,7 @@ import { OTPcard } from "@/Client-Components/auth-page/OTPcardClient";
 
 export default function(){
     return(
-        <div className="flex flex-col">
+        <div className="flex flex-col bg-slate-900">
             < OTPcard />
         </div>
     )
