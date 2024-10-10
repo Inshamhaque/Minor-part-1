d8ff994fc05e9d7d0108295bbd4ded072627fa80
Insham Haque <haqueinsham@gmail.com>
2024-10-10 22:02:33 +0530

    redis

--- app/api/redis/route.ts
+++ app/api/redis/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 //connecting to redis client
 import { Redis } from '@upstash/redis';
 
-
+// TODO:  try to use this in the process.env ... not working fine 
 const redis = new Redis({
   url: 'https://romantic-kodiak-24878.upstash.io',
   token: 'AWEuAAIjcDFmNWI2ZTQ3OWRhZjQ0YzI1OTdlZTcwM2I2Y2Q3ZDNhYXAxMA',
